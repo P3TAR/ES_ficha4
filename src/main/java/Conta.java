c20a30a535f2b75759679ab388f2e80dd35441aa
P3TAR <73306402+P3TAR@users.noreply.github.com>
2023-03-28 13:47:43 +0100

    Add files via upload

---
+++ src/main/java/Conta.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Conta {
+    private String nome;
+    private String IBAN;
+    private List<transferencia> transferencias;
+    private long valor_mensal;
+    private long gasto_maximo;
+    private long gasto_total;
+
+}
---
+++ src/main/java/transferencia.java
@@ -0,0 +1,6 @@
+public class transferencia {
+    private String IBAN;
+    private long valor;
+
+
+}
